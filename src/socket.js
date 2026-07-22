bc860ef15a9b916214a08c1d88bfb680b0ed2c2a
marksteward16 <MarkS@synkronous.com>
2026-07-22 20:03:13 +0800

    Simplify socket URL configuration by removing fallback to local server

--- src/socket.js
+++ src/socket.js
@@ -1,9 +1,8 @@
 import { io } from "socket.io-client";
 
 const socketUrl =
-  import.meta.env.VITE_SOCKET_URL ||
-  "https://kpbackend-ashy.vercel.app/" ||
-  `${window.location.protocol}//${window.location.hostname}:4000`;
+  import.meta.env.VITE_SOCKET_URL || "https://kpbackend-ashy.vercel.app/";
+//|| `${window.location.protocol}//${window.location.hostname}:4000`;
 
 const socket = io(socketUrl, { autoConnect: false });
 
